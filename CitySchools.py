f6669fcb0b55ce3ffab5df722a3d2539109b46a1
David Jaimes <david@djaimes.com>
2019-12-07 19:01:56 -0800

    comments in top performing and worst performing schools

--- CitySchools.py
+++ CitySchools.py
@@ -9,14 +9,22 @@ students = pd.read_csv("Resources/students_complete.csv")
 # Combine the data into a single dataset
 data = pd.merge(students, schools, how="left", on="school_name")
 
+# Create group by school name and use keys() to get list of school names
 group = data.groupby(['school_name'])
 school_name = group.groups.keys()
+
+# Use explode() function to unflatten list of lists
 school_type = group['type'].unique().explode()
 total_students = group.size()
 total_budget = group['budget'].unique().explode()
-budget_per_student = np.array(total_budget) / total_students
+
+# Note: Not allowed to divide list array by pd.Series
+budget_per_student = total_budget / total_students
 average_math = group['math_score'].mean()
 average_reading = group['reading_score'].mean()
+
+# Take each group array, set condition, and sum up all True values in array.
+# Also, underscore is used because key in group is not being used.
 passing_math = np.array([np.sum(val >= 70) for (_, val) in group['math_score']])
 passing_reading = np.array([np.sum(val >= 70) for (_, val) in group['reading_score']])
 overall_passing = (passing_math + passing_reading) / 2
@@ -33,4 +41,4 @@ school_summary = pd.DataFrame({
     '% Passing Reading': 100 * passing_reading / total_students,
     '% Overall Passing Rate': 100 * overall_passing / total_students
 }, index=school_name)
-print(school_summary.sort_values(by='% Overall Passing Rate', ascending=False).head(5))
+print(school_summary.sort_values(by='% Overall Passing Rate', ascending=True).head(5))
